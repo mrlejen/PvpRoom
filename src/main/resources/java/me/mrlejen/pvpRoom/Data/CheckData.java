0d7f3c7a76a4a71382a24cbb40c036b195127cf5
MrLejen <162735748+mrlejen12@users.noreply.github.com>
2025-04-04 02:00:39 +0300

    Create CheckData.java

---
+++ src/main/resources/java/me/mrlejen/pvpRoom/Data/CheckData.java
@@ -0,0 +1,11 @@
+package me.mrlejen.pvpRoom.Data;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class CheckData {
+    public static final HashMap<UUID, Boolean> first = new HashMap<>();
+    public static HashMap<UUID, Boolean> getcheckdata() {
+        return first;
+    }
+}
